15777a5909138bdddfe9d08ab907fa79231f4a01
nycolemedina-lab <nycole.medina@gmain.com>
2026-08-21 11:15:43 -0400

    add tool.c file, move helper functions to tool file

--- _printf.c
+++ _printf.c
@@ -1,68 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
 
-int conv_char(va_list args)
-{
-	char c;
-
-	c = va_arg(args, int);
-	_putchar(c);
-	return (1);
-}
-
-int conv_string(va_list args)
-{
-	int i = 0;
-	char *str = va_arg(args, char *);
-
-	if (str == NULL)
-		str = "(null)";
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	return (i);
-}
-
-int conv_perc(va_list args)
-{
-	(void)args;
-	_putchar('%');
-	return (1);
-}
-
-int print_num(unsigned int n)
-{
-	int count = 0;
-
-	if (n / 10)
-		count += print_num(n / 10);
-	_putchar((n % 10) + '0');
-	return (count + 1);
-}
-
-int conv_int(va_list args)
-{
-	int n = va_arg(args, int);
-	int count = 0;
-	unsigned int num;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		count++;
-		num = -n;
-	}
-	else
-	{
-		num = n;
-	}
-	count += print_num(num);
-	return (count);
-}
-
+/**
+*
+*
+*
+*
+*
+*
+*/
 
 int _printf(const char *format, ...)
 {
--- main.h
+++ main.h
@@ -3,6 +3,7 @@
 
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
 
 /**
 * struct specifier - structure to map format specifiers to printer functions
